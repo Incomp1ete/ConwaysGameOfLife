2e646f15e91f318aba45fa2aa118cbbfa92d4d7f
Incomp1ete <150806775+Incomp1ete@users.noreply.github.com>
2026-06-21 10:46:24 +0100

    Game Cointrol Buttons

--- src/GameOfLife.c
+++ src/GameOfLife.c
@@ -20,8 +20,28 @@
 void displayGame(struct nk_context *ctx){
     if(nk_begin(ctx, "Game Of Life", nk_rect(0, 0, WINDOWS_WIDTH, WINDOW_HEIGHT),
     NK_WINDOW_BORDER | NK_WINDOW_MOVABLE)){
-        nk_layout_row_dynamic(ctx, 30, 1);
-        nk_label(ctx, "Hello World", NK_TEXT_LEFT);
+        nk_layout_row_begin(ctx, NK_STATIC, 30, 4);
+        float buttonWidth = 30;
+
+        nk_layout_row_push(ctx, buttonWidth);
+        if(nk_button_symbol(ctx, NK_SYMBOL_RECT_SOLID)){
+            // Stop
+        }
+
+        nk_layout_row_push(ctx, buttonWidth);
+        if(nk_button_symbol(ctx, NK_SYMBOL_TRIANGLE_DOWN_OUTLINE)){
+            // Pause
+        }
+
+        nk_layout_row_push(ctx, buttonWidth);
+        if(nk_button_symbol(ctx, NK_SYMBOL_TRIANGLE_RIGHT)){
+            // Play
+        }
+
+        nk_layout_row_push(ctx, buttonWidth);
+        if(nk_button_symbol(ctx, NK_SYMBOL_TRIANGLE_RIGHT_OUTLINE)){
+            // Frame
+        }
     }
 }
 
