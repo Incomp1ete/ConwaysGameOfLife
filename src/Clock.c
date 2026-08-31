7eda9b3ab7c393ae9989d8f6ba7011bddb0c13a4
Incom1ete <150806775+Incomp1ete@users.noreply.github.com>
2026-08-31 08:53:12 +0200

    Clock for Win32 implemented

--- src/Clock.c
+++ src/Clock.c
@@ -1,5 +1,57 @@
 #include "Clock.h"
 #include <stdlib.h>
+
+
+#if defined(_WIN32)
+    #include <windows.h>
+
+    struct Timer {
+        LARGE_INTEGER freq;
+        LARGE_INTEGER start;
+        LARGE_INTEGER end;
+        double waitInMs;
+    };
+
+    int timer_init(Timer** timer, double waitInSeconds){
+        *timer = malloc(sizeof(Timer));
+        if(*timer == NULL){
+            return -1;
+        }
+
+        QueryPerformanceFrequency(&((*timer)->freq));
+        timeBeginPeriod((int) waitInSeconds);
+        (*timer)->waitInMs = 1000.0 * waitInSeconds;
+        return 0;
+    }
+
+    int timer_restart(Timer* timer){
+        QueryPerformanceCounter(&(timer->start));
+        return 0;
+    }
+
+    int timer_sleepTillDeadline(Timer* timer){
+        LARGE_INTEGER end;
+        QueryPerformanceCounter(&end);
+
+        double elapsedMs = (double)(end.QuadPart - timer->start.QuadPart)
+                            * 1000.0 / timer->freq.QuadPart;
+        double remainingMs = (timer->waitInMs) - elapsedMs;
+
+        if(remainingMs > 0){
+            Sleep((DWORD)remainingMs);
+        }
+
+        return 0;
+    }
+
+    void timer_destroy(Timer* timer){
+        timeEndPeriod((timer->waitInMs)/1000);
+        free(timer);
+    }
+#elif defined(__unix__)
+#include <unistd.h> // Needed for _POSIX_VERSION guard
+
+#if defined(_POSIX_VERSION)
 #include <time.h>
 
 struct Timer {
@@ -48,3 +100,15 @@ int timer_sleepTillDeadline(Timer* timer){
 void timer_destroy(Timer* timer){
     free(timer);
 }
+
+#else
+
+#error "unsupported platform"
+
+#endif
+
+#else
+
+#error "unsupported platform"
+
+#endif
