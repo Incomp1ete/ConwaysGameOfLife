27140f54094df90aa51aa8f8b49c27222c5c6a96
Incom1ete <150806775+Incomp1ete@users.noreply.github.com>
2026-09-01 12:41:05 +0200

    Implemented threading for win32

--- src/Threading.c
+++ src/Threading.c
@@ -1,5 +1,123 @@
 #include "Threading.h"
 #include <stdlib.h>
+
+#if defined(_WIN32)
+#include <windows.h>
+
+struct Mutex {
+    CRITICAL_SECTION cs;
+};
+
+int mutex_create(Mutex** mutex){
+    *mutex = malloc(sizeof(Mutex));
+    if(*mutex == NULL){
+        return -1;
+    }
+
+    InitializeCriticalSection(&(*mutex)->cs);
+    return 0;
+}
+
+int mutex_lock(Mutex* mutex){
+    EnterCriticalSection(&(mutex->cs));
+    return 0;
+}
+
+int mutex_unlock(Mutex* mutex){
+    LeaveCriticalSection(&(mutex->cs));
+    return 0;
+}
+
+int mutex_destroy(Mutex* mutex){
+    DeleteCriticalSection(&(mutex->cs));
+    free(mutex);
+    return 0;
+}
+
+struct Cond{
+    CONDITION_VARIABLE cv;
+};
+
+int cond_create(Cond** cond){
+    *cond = malloc(sizeof(Cond));
+    if(*cond == NULL){
+        return -1;
+    }
+
+    InitializeConditionVariable(&((*cond)->cv));
+    return 0;
+}
+
+int cond_signal(Cond* cond){
+    WakeConditionVariable(&(cond->cv));
+    return 0;
+}
+
+int cond_wait(Cond* cond, Mutex* mutex){
+    SleepConditionVariableCS(&(cond->cv), &(mutex->cs), INFINITE);
+    return 0;
+}
+
+int cond_broadcast(Cond* cond){
+    WakeAllConditionVariable(&(cond->cv));
+    return 0;
+}
+
+int cond_destroy(Cond* cond){
+    free(cond);
+    return 0;
+}
+
+typedef struct WorkerWrapper {
+    void *(*func)(void *);
+} WorkerWrapper;
+
+struct Thread{
+    HANDLE thread;
+};
+
+DWORD WINAPI Worker(LPVOID lpParam){
+    WorkerWrapper* workerWrapper = (WorkerWrapper*)lpParam;
+    workerWrapper->func(NULL);
+    free(workerWrapper);
+    return 0;
+}
+
+int thread_create(Thread** thread, void *(*func)(void *)){
+    *thread = malloc(sizeof(Thread));
+    if(*thread == NULL){
+        return -1;
+    }
+
+    WorkerWrapper* workerWrapper = malloc(sizeof(WorkerWrapper));
+    if(workerWrapper == NULL){
+        free(*thread);
+        return -1;
+    }
+    workerWrapper->func = func;
+    
+    (*thread)->thread = CreateThread(NULL, 0, Worker, workerWrapper, 0, NULL);
+    if((*thread)->thread == NULL){
+        free(*thread);
+        free(workerWrapper);
+        return -1;
+    }
+
+    return 0;
+}
+
+int thread_join(Thread* thread){
+    WaitForSingleObject(thread->thread, INFINITE);
+    CloseHandle(thread->thread);
+    free(thread);
+    return 0;
+}
+
+
+#elif defined(__unix__)
+#include <unistd.h>
+
+#if defined(_POSIX_VERSION)
 #include <pthread.h>
 
 struct Mutex
@@ -106,3 +224,15 @@ int thread_join(Thread* thread){
     
     return code;
 }
+
+#else
+
+#error "unsupported platform"
+
+#endif
+
+#else
+
+#error "unsupported platform"
+
+#endif
